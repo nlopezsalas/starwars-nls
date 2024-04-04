5093fc6ef974942e1547da8d00944da4ae00fd71
Natalia <nlopezsalas@gmail.com>
2024-04-04 20:58:48 +0000

    C

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -17,31 +17,80 @@ const getState = ({ getStore, getActions, setStore }) => {
 		actions: {
 			getSWAPIResource: async (object) => {
 				try {
+					const isSpecificResource = object.split('/').length > 1; // Verifica si es un recurso específico como '/people/1'
+					const objectType = object.split('/')[0]; // 'people', 'planets', etc.
+					const resourceId = object.split('/')[1]; // ID del recurso, si está presente
+			
+					// Determina la clave para localStorage y store
+					const storageKey = isSpecificResource && objectType === 'people' ? `character/${resourceId}` : object;
+					const cacheKey = `swapi_${storageKey}`;
+					
+					// Intenta recuperar de localStorage
+					const cachedData = localStorage.getItem(cacheKey);
+					if (cachedData) {
+						// Si hay datos en caché, usa esos
+						const parsedData = JSON.parse(cachedData);
+						setStore({ [storageKey]: parsedData });
+						return; // Termina la ejecución si se encuentran datos en caché
+					}
+			
+					// Configuración para la solicitud a la API
 					const myHeaders = new Headers();
 					myHeaders.append("Content-Type", "application/json");
-
 					const requestOptions = {
 						method: "GET",
 						headers: myHeaders,
 					};
-					let response = await fetch(`https://www.swapi.tech/api/${object}`, requestOptions)
-					let result = await response.json()
-					console.log(result);
-					if (result.hasOwnProperty('results'))
-						// Para '/people', '/planets', '/vehicles'
-						setStore({ [object]: result.results })
-					else {
-						// Para '/people/1', '/planets/1', '/vehicles/1'
-						const objectType = object.split('/')[0].replace(/s$/, '');
-						console.log(`%c${objectType}`, 'color: white; background-color: green;');
-						const resourceName = (objectType === "people") ? "character" : objectType;
-						setStore({ [resourceName]: result.result.properties })
+			
+					// Realiza la solicitud a la API
+					let response = await fetch(`https://www.swapi.tech/api/${object}`, requestOptions);
+					let result = await response.json();
+			
+					// Guarda los datos en localStorage y actualiza el store
+					localStorage.setItem(cacheKey, JSON.stringify(result.results || result.result.properties));
+					if (result.hasOwnProperty('results')) {
+						// Para listados como '/people', '/planets', '/vehicles'
+						setStore({ [objectType]: result.results });
+					} else {
+						// Para recursos específicos como '/people/1'
+						const resourceName = isSpecificResource && objectType === 'people' ? 'character' : objectType;
+						setStore({ [resourceName]: result.result.properties });
 					}
-
+			
 				} catch (error) {
-					console.error(error)
+					console.error('Fetch error:', error);
 				}
 			},
+			
+			
+			
+			// getSWAPIResource: async (object) => {
+			// 	try {
+			// 		const myHeaders = new Headers();
+			// 		myHeaders.append("Content-Type", "application/json");
+
+			// 		const requestOptions = {
+			// 			method: "GET",
+			// 			headers: myHeaders,
+			// 		};
+			// 		let response = await fetch(`https://www.swapi.tech/api/${object}`, requestOptions)
+			// 		let result = await response.json()
+			// 		console.log(result);
+			// 		if (result.hasOwnProperty('results'))
+			// 			// Para '/people', '/planets', '/vehicles'
+			// 			setStore({ [object]: result.results })
+			// 		else {
+			// 			// Para '/people/1', '/planets/1', '/vehicles/1'
+			// 			const objectType = object.split('/')[0].replace(/s$/, '');
+			// 			// console.log(`%c${objectType}`, 'color: white; background-color: green;');
+			// 			const resourceName = (objectType === "people") ? "character" : objectType;
+			// 			setStore({ [resourceName]: result.result.properties })
+			// 		}
+
+			// 	} catch (error) {
+			// 		console.error(error)
+			// 	}
+			// },
 			addFavorites: (name, uid, resource) => {
 				const newFavorite = {
 					name: name,
--- src/js/views/singleResource.js
+++ src/js/views/singleResource.js
@@ -12,8 +12,11 @@ import Image from 'react-bootstrap/Image';
 export const SingleResource = props => {
 	const { store, actions } = useContext(Context);
 	const params = useParams();
-	console.log(`%c${params.resource}`,'color: red');
-	const resources = params.resource; 
+	console.log(`%c${params.resource}`, 'color: red');
+	const resources = params.resource;
+
+	const [resourceData, setResourceData] = useState(null);
+
 
 	//obtener el recurso
 	let resource = '';
@@ -26,57 +29,87 @@ export const SingleResource = props => {
 		}
 	}
 
+	const imageResource = resources === 'people' ? 'characters' : resources;
+
 	useEffect(() => {
-		actions.getSWAPIResource(`${resources}/${params.theid}`);
-		console.log('Resource:', resources); 
-	}, []);
+		const resourceKey = `swapi_${params.resource}/${params.theid}`;
+		const cachedData = localStorage.getItem(resourceKey);
+
+		if (cachedData) {
+			setResourceData(JSON.parse(cachedData));
+		} else {
+			actions.getSWAPIResource(`${resources}/${params.theid}`)
+				.then(data => {
+					setResourceData(data); // Asegúrate de que esto sea lo que esperas
+				})
+				.catch(error => {
+					console.error("Error fetching resource:", error);
+					// Manejar el error como consideres apropiado
+				});
+		}
+	}, [params.resource, params.theid, actions]);
+
 	console.log(resources);
 	console.log(resource);
+	
 	return (
 		<div className="container mt-5">
 			<Container>
 				<Row>
 					<Col>
-						{(resources === 'people') 
-							?<Image src={`https://starwars-visualguide.com/assets/img/${resource+'s'}/${params.theid}.jpg`} /> 
-							: (store[resource].name === 'Tatooine') 
-							? <Image src={`https://starwars-visualguide.com/assets/img/${resources}/2.jpg`} /> 
-							: <Image src={`https://starwars-visualguide.com/assets/img/${resources}/${params.theid}.jpg`} />
-						}
+						{/* Imagen: misma para ambas vistas */}
+						<Image src={`https://starwars-visualguide.com/assets/img/${imageResource}/${params.theid}.jpg`} />
 					</Col>
 					<Col>
-						<h1>{store[resource].name}</h1>
-						<p>Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in massa egestas mollis varius;
-							dignissim elementum. Mollis tincidunt mattis hendrerit dolor eros enim, nisi ligula ornare.
-							Hendrerit parturient habitant pharetra rutrum gravida porttitor eros feugiat. Mollis elit
-							sodales taciti duis praesent id. Consequat urna vitae morbi nunc congue.</p>
+						{/* Nombre: varía según si resourceData es null o no */}
+						<h1>{resourceData ? resourceData.name : store[resource].name}</h1>
+						{/* Párrafo estático, igual para ambas vistas */}
+						<p>Lorem ipsum odor amet...</p>
 					</Col>
 				</Row>
 				<Row className="border-top mt-2 pt-3">
 					<div className="d-flex flex-fill flex-wrap gap-3">
-						{Object.keys(store[resource]).map((key, index) => {
-							if (key !== "name" &&
-								typeof store[resource][key] !== 'object' &&
-								store[resource][key] !== '' &&
-								!store[resource][key].startsWith('http')) {
-								return (
-									<React.Fragment key={key}>
-										<Col className="">
-											<div className="d-inline-block">
-												<strong>{key.replace(/_/g, ' ').charAt(0).toUpperCase() + key.replace(/_/g, ' ').slice(1)}:</strong>
-											</div>
-											<div className="d-block">
-												{store[resource][key]}
-											</div>
-										</Col>
-										{(index + 1) % 5 === 0 && <div className="w-100"></div>}
-									</React.Fragment>
-								);
-							}
-							return null;
-						})}
+						{/* Datos dinámicos: varían según si resourceData es null o no */}
+						{resourceData ? (
+							Object.keys(resourceData).map((key, index) => {
+								if (key !== "name" && typeof resourceData[key] !== 'object' && resourceData[key] !== '' && !resourceData[key].startsWith('http')) {
+									return (
+										<React.Fragment key={key}>
+											<Col>
+												<div className="d-inline-block">
+													<strong>{key.replace(/_/g, ' ').charAt(0).toUpperCase() + key.replace(/_/g, ' ').slice(1)}:</strong>
+												</div>
+												<div className="d-block">
+													{resourceData[key]}
+												</div>
+											</Col>
+											{(index + 1) % 5 === 0 && <div className="w-100"></div>}
+										</React.Fragment>
+									);
+								}
+								return null;
+							})
+						) : (
+							Object.keys(store[resource]).map((key, index) => {
+								if (key !== "name" && typeof store[resource][key] !== 'object' && store[resource][key] !== '' && !store[resource][key].startsWith('http')) {
+									return (
+										<React.Fragment key={key}>
+											<Col>
+												<div className="d-inline-block">
+													<strong>{key.replace(/_/g, ' ').charAt(0).toUpperCase() + key.replace(/_/g, ' ').slice(1)}:</strong>
+												</div>
+												<div className="d-block">
+													{store[resource][key]}
+												</div>
+											</Col>
+											{(index + 1) % 5 === 0 && <div className="w-100"></div>}
+										</React.Fragment>
+									);
+								}
+								return null;
+							})
+						)}
 					</div>
-
 				</Row>
 			</Container>
 
@@ -85,7 +118,7 @@ export const SingleResource = props => {
 					Back home
 				</span>
 			</Link>
-		</div >
+		</div>
 	);
 };
 
